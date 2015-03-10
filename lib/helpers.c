ea9685913b2c890411f312891e4f6c6db36faf46
lightning95 <aydar.gizatullin@gmail.com>
2015-03-11 00:45:58 +0300

    helpers.c

---
+++ lib/helpers.c
@@ -0,0 +1,20 @@
+#include "helpers.h"
+#include <unistd.h>
+
+ssize_t read_(int fd, void* buf, size_t count) 
+{
+    size_t offset = 0;
+    ssize_t rr;
+    while(rr = read(fd, buf + offset, count - offset) > 0) 
+        offset += rr;
+    return rr == -1 ? -1 : offset;        
+}
+
+ssize_t write_(int fd, const void* buf, size_t count) 
+{
+    size_t offset = 0;
+    ssize_t rr = 0;
+    while (offset < count && rr >= 0) 
+        offset += rr = write(fd, buf + offset, count - offset);
+    return rr == -1 ? -1 : offset;
+}
