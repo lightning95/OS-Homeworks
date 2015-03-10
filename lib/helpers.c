bd80a2228f7ddee3b199a2fe038b017378a3ac9b
lightning95 <aydar.gizatullin@gmail.com>
2015-03-11 01:21:18 +0300

    terrible bug fixed

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,11 +1,11 @@
-#include "helpers.h"
 #include <unistd.h>
+#include "helpers.h"
 
 ssize_t read_(int fd, void* buf, size_t count) 
 {
     size_t offset = 0;
     ssize_t rr;
-    while(rr = read(fd, buf + offset, count - offset) > 0) 
+    while ((rr = read(fd, buf + offset, count - offset)) > 0) 
         offset += rr;
     return rr == -1 ? -1 : offset;        
 }
